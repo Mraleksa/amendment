866e74331f21cb13393e737d02858c772def734f
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-04 18:46:23 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -59,7 +59,7 @@ if(change=="itemPriceVariation"){
 
   // Create new table
   //db.run("DELETE FROM data");
-  db.run("CREATE TABLE data (dateModified TEXT,tenderID TEXT,procuringEntity TEXT,change TEXT,numberOfBids INT,amount INT,cpv TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,tenderID TEXT,procuringEntity TEXT,change TEXT,numberOfBids INT,amount INT,cpv TEXT)");
 
   
   // Insert a new record
