bbca62c07369e9dfef00ba0222d61f852cb0628e
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-04 18:21:54 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,5 +1,5 @@
 var client = require('http-api-client');
-var d3 = require("d3");
+//var d3 = require("d3");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("data.sqlite");
 
