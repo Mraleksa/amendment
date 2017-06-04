5eafe88b3628ae771a863448877801defa0c4af9
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-04 21:42:46 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ var formatTime = d3.timeFormat("%Y-%m-%d");
 
 var myDate = new Date();
 var dayOfMonth = myDate.getDate();
-myDate.setDate(dayOfMonth - 2);
+myDate.setDate(dayOfMonth - 1);
 
 var start  = formatTime(myDate);
 console.log(start);
@@ -56,21 +56,20 @@ if(change=="itemPriceVariation"){
 	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+tender_id})
 					.then(function (data) {
 						
-				db.serialize(function() {
+db.serialize(function() {
 
-  // Create new table
-  //db.run("DELETE FROM data");
-  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,tenderID TEXT,procuringEntity TEXT,change TEXT,numberOfBids INT,amount INT,cpv TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,suppliers TEXT,numberOfBids INT,amount INT,cpv TEXT)");
 
+  //data.getJSON().data.dateSigned когда заключен контракт
+  //data.getJSON().data.suppliers.name поставщик
   
   // Insert a new record
-  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?)");
+  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)");
   
-  statement.run(item.dateModified,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,change,data.getJSON().data.numberOfBids,data.getJSON().data.value.amount,data.getJSON().data.items[0].classification.description);
+  statement.run(item.dateModified,data.getJSON().data.dateSigned,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.suppliers.name,data.getJSON().data.numberOfBids,data.getJSON().data.value.amount,data.getJSON().data.items[0].classification.description);
   
   statement.finalize();
 });
-
 		
 						
 	/*							
