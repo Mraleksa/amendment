94ffdcfe6fa4556cd10fe0c5cef53fb756921c61
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-05 08:04:50 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ var formatTime = d3.timeFormat("%Y-%m-%d");
 
 var myDate = new Date();
 var dayOfMonth = myDate.getDate();
-myDate.setDate(dayOfMonth - 3);
+myDate.setDate(dayOfMonth - 2);
 
 var start  = formatTime(myDate);
 console.log(start);
@@ -57,16 +57,12 @@ if(change=="itemPriceVariation"){
 						
 db.serialize(function() {
 	
-  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,suppliers TEXT,numberOfBids INT,amount INT,cpv TEXT)");
-console.log(change)
-  //data.getJSON().data.dateSigned когда заключен контракт
-  //data.getJSON().data.suppliers.name поставщик
+  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,amount INT,cpv TEXT)");
+
+  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?)");
   
-  // Insert a new record
-  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)");
-  
-  statement.run(item.dateModified,data.getJSON().data.dateSigned,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.suppliers.name,data.getJSON().data.numberOfBids,data.getJSON().data.value.amount,data.getJSON().data.items[0].classification.description);
-  console.log(item.dateModified)
+  statement.run(item.dateModified,data.getJSON().data.dateSigned,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,data.getJSON().data.value.amount,data.getJSON().data.items[0].classification.description);
+
   statement.finalize();
 });
 		
@@ -102,4 +98,3 @@ console.log(change)
 }
 
 piv ();	
- 
