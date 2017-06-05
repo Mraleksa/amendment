f7652d4fe374419a14e91a4562a22894798f81a9
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-05 08:06:47 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,7 +3,7 @@ var d3 = require("d3");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("data.sqlite");
 
-//db.run("DELETE FROM data");
+db.run("DELETE FROM data");
 
 var p = 0;
 
