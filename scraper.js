c0bbacc5f6631f01b5fc8aec6a7c92de0d90bf28
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-05 08:07:41 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ var formatTime = d3.timeFormat("%Y-%m-%d");
 
 var myDate = new Date();
 var dayOfMonth = myDate.getDate();
-myDate.setDate(dayOfMonth - 7);
+myDate.setDate(dayOfMonth - 2);
 
 var start  = formatTime(myDate);
 console.log(start);
