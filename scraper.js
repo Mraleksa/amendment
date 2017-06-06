a5f86d9a521b8a913eb8f6747e1cf37b834bde35
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-06 17:02:45 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -8,11 +8,14 @@ var p = 0;
 var formatTime = d3.timeFormat("%Y-%m-%d");
 var myDate = new Date();
 var dayOfMonth = myDate.getDate();
-myDate.setDate(dayOfMonth - 4);
+myDate.setDate(dayOfMonth - 3);
 var start  = formatTime(myDate);
 console.log(start);
 var end  = formatTime(new Date());
 console.log(end);
+ 
+
+
  
 function piv(){  
 p++;
@@ -33,28 +36,40 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 var change = data.getJSON().data.changes[data.getJSON().data.changes.length-1].rationaleTypes[0];
 if(change=="itemPriceVariation"){
 	
-	//var lotIdContracts = data.getJSON().data.items[0].relatedLot;
+	var lotIdContracts = data.getJSON().data.items[0].relatedLot;
 	
 	
 	var dateSigned = data.getJSON().data.dateSigned;
 	var amount = data.getJSON().data.value.amount;
-	var tender_id = data.getJSON().data.tender_id;
 	
-	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+tender_id})
+	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+data.getJSON().data.tender_id})
 					.then(function (data) {
 		
-	//for (var i = 1; i <= data.getJSON().data.lots.length; i++) {
-	//		if(lotIdContracts==data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].id){var startAmount =  data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].value.amount};
-			
-	 //  }
-	//var save = (startAmount-amount)/startAmount*100;
-		var save = 100;
+	var startAmount;
 		
+	if(data.getJSON().data.lots==undefined){
+	startAmount = data.getJSON().data.value.amount;
+	console.log(startAmount)
+	
+	}
+	else {
+	for (var i = 1; i <= data.getJSON().data.lots.length; i++) {
+			if(lotIdContracts==data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].id){
+				startAmount =  data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].value.amount
+			};			
+	   }
+	   
+	console.log(startAmount)
+	}
+
+
+
+	
 	db.serialize(function() {	
-	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,save INT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,amount INT,cpv TEXT)");
+	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,cpv TEXT)");
 	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)");
   	
-	statement.run(item.dateModified,dateSigned,save,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,amount,data.getJSON().data.items[0].classification.description);
+	statement.run(item.dateModified,dateSigned,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,startAmount,amount,data.getJSON().data.items[0].classification.description);
 	//console.log(change);
 	statement.finalize();
 	});
