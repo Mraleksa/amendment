659369d664fcf8921ffb87b17d2b785c3b972ba4
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-06 07:01:16 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -33,7 +33,7 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 var change = data.getJSON().data.changes[data.getJSON().data.changes.length-1].rationaleTypes[0];
 if(change=="itemPriceVariation"){
 	
-	var lotIdContracts = data.getJSON().data.items[0].relatedLot;
+	//var lotIdContracts = data.getJSON().data.items[0].relatedLot;
 	
 	
 	var dateSigned = data.getJSON().data.dateSigned;
@@ -43,12 +43,12 @@ if(change=="itemPriceVariation"){
 	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+tender_id})
 					.then(function (data) {
 		
-	for (var i = 1; i <= data.getJSON().data.lots.length; i++) {
-			if(lotIdContracts==data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].id){var startAmount =  data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].value.amount};
+	//for (var i = 1; i <= data.getJSON().data.lots.length; i++) {
+	//		if(lotIdContracts==data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].id){var startAmount =  data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].value.amount};
 			
 	   }
-	var save = (startAmount-amount)/startAmount*100;
-		
+	//var save = (startAmount-amount)/startAmount*100;
+		var save = 100;
 		
 	db.serialize(function() {	
 	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,save INT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,amount INT,cpv TEXT)");
